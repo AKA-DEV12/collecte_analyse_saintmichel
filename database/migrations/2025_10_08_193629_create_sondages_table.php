01ee8b7b29c0dfa6a00909de49f3668a4925d104
AKA-DEV12 <aandrekoffi960@gmail.com>
2025-10-19 04:28:11 +0000

    ajout de paramtre de deploiement pour render

--- database/migrations/2025_10_08_193629_create_sondages_table.php
+++ database/migrations/2025_10_08_193629_create_sondages_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('sondages', function (Blueprint $table) {
             $table->id();
             $table->string('titre');
+            $table->integer('agent');
             $table->text('description')->nullable();
             $table->timestamps();
         });
